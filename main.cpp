4f1ed6a8c3a3090ad92ce2c1ec9f7d62662e7df5
Lauri Kasanen <cand@gmx.com>
2020-11-08 17:35:49 +0200

    more

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <lrtypes.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <map>
 #include <vector>
@@ -10,6 +11,21 @@ using namespace std;
 static void die(const char why[]) {
 	printf("%s\n", why);
 	exit(1);
+}
+
+struct entry {
+	vector<u8> data;
+};
+
+static int lencmp(const void *ap, const void *bp) {
+	const entry * const a = (entry *) ap;
+	const entry * const b = (entry *) bp;
+
+	if (a->data.size() > b->data.size())
+		return -1;
+	if (a->data.size() < b->data.size())
+		return 1;
+	return 0;
 }
 
 int main(int argc, char **argv) {
@@ -34,8 +50,27 @@ int main(int argc, char **argv) {
 
 	// Analyze
 	map<vector<u8>, u32> stuff[129];
+	u32 maxes[129];
+	u8 lastlevel = 128;
 
-	u8 i;
+	u8 bytes[256] = { 0 };
+
+	u32 i;
+	for (i = 0; i < len; i++) {
+		bytes[mem[i]] = 1;
+	}
+
+	entry entries[256];
+
+	u32 used = 0;
+	for (i = 0; i < 256; i++) {
+		if (bytes[i]) {
+			entries[used].data.push_back(i);
+			used++;
+		}
+	}
+	printf("%u values used\n", used);
+
 	for (i = 2; i <= 128; i++) {
 		u32 p;
 		u32 max = 0;
@@ -66,11 +101,203 @@ int main(int argc, char **argv) {
 		}*/
 
 		printf("Largest amount was %u\n", max);
+		maxes[i] = max;
 
-		if (max == 1)
+		if (max == 1) {
+			lastlevel = i;
 			break;
+		}
 	}
 
+	puts("");
+
+	u32 bestamount = 0;
+	u8 best = 0;
+
+	for (i = 2; i <= lastlevel; i++) {
+		const u32 mul = i * maxes[i];
+		if (mul > bestamount) {
+			best = i;
+			bestamount = mul;
+		}
+	}
+
+	printf("Best amount was %u, at %u\n", bestamount, best);
+
+	u32 numentries = used + 1;
+
+	map<vector<u8>, u32>::const_iterator it;
+	for (it = stuff[best].begin(); it != stuff[best].end(); it++) {
+		if (it->second != maxes[best])
+			continue;
+
+		entries[used].data = it->first;
+
+		printf("Contents: ");
+		for (i = 0; i < it->first.size(); i++) {
+			printf("%u,", it->first[i]);
+		}
+		puts("");
+		break;
+	}
+
+	// Erase them from the memmap
+	u8 * const memmap = (u8 *) calloc(1, len);
+	u32 erased = 0;
+
+	for (i = 0; i < len - best; i++) {
+		if (!memcmp(&entries[used].data[0], &mem[i], best)) {
+			memset(&memmap[i], 1, best);
+			i += best - 1;
+			erased += best;
+		}
+	}
+
+	// We have our first entry. Time to iterate
+	while (numentries < 256) {
+		printf("Iterating. %u entries found, %u/%u bytes\n",
+			numentries, erased, len);
+
+		memset(maxes, 0, 129);
+
+		for (i = 2; i <= lastlevel; i++) {
+			u32 p;
+			u32 max = 0;
+			stuff[i].clear();
+			for (p = 0; p < len; p++) {
+				if (p + i >= len)
+					break;
+
+				if (u8 *ptr = (u8 *) memrchr(&memmap[p], 1, i)) {
+					const u32 dist = ptr - &memmap[p];
+					if (dist)
+						p += dist - 1;
+					continue;
+				}
+
+				vector<u8> vec;
+				u8 k;
+				for (k = 0; k < i; k++)
+					vec.push_back(mem[p + k]);
+
+				const u32 one = ++stuff[i][vec];
+				if (one > max)
+					max = one;
+			}
+			printf("Length %u had %lu unique matches, max %u\n", i,
+				stuff[i].size(), max);
+
+			maxes[i] = max;
+
+			if (max == 1) {
+				lastlevel = i;
+				break;
+			} else if (!max) {
+				lastlevel = i - 1;
+				break;
+			}
+		}
+
+		bestamount = 0;
+		best = 0;
+
+		for (i = 2; i <= lastlevel; i++) {
+			const u32 mul = i * maxes[i];
+			if (mul > bestamount) {
+				best = i;
+				bestamount = mul;
+			}
+		}
+		printf("Best amount was %u, at %u\n", bestamount, best);
+
+		// Found a new best
+		for (it = stuff[best].begin(); it != stuff[best].end(); it++) {
+			if (it->second != maxes[best])
+				continue;
+
+			entries[numentries++].data = it->first;
+
+			printf("Contents: ");
+			for (i = 0; i < it->first.size(); i++) {
+				printf("%u,", it->first[i]);
+			}
+			puts("");
+			break;
+		}
+
+		// Erase from map
+		for (i = 0; i < len - best; i++) {
+
+			if (u8 *ptr = (u8 *) memrchr(&memmap[i], 1, best)) {
+				const u32 dist = ptr - &memmap[i];
+				if (dist)
+					i += dist - 1;
+				continue;
+			}
+
+			if (!memcmp(&entries[numentries - 1].data[0], &mem[i], best)) {
+				memset(&memmap[i], 1, best);
+				i += best - 1;
+				erased += best;
+			}
+		}
+
+		// End conditions
+		// If longest open streak is 1
+		if (lastlevel == 1) {
+			puts("No more runs to find");
+			break;
+		}
+
+		// If dict length exceeds 512
+		u32 dlen = 0;
+		for (i = 0; i < numentries; i++)
+			dlen += entries[i].data.size();
+		if (dlen >= 512) {
+			puts("Dictionary length capped");
+			break;
+		}
+	}
+
+	puts("");
+
+	u32 dlen = 0;
+	for (i = 0; i < numentries; i++)
+		dlen += entries[i].data.size();
+	printf("Dictionary has %u entries, %u total length.\n", numentries, dlen);
+
+	qsort(entries, numentries, sizeof(entry), lencmp);
+
+	u32 k;
+	for (i = 0; i < numentries; i++) {
+		printf("Entry %u: len %lu\n\t", i, entries[i].data.size());
+		for (k = 0; k < entries[i].data.size(); k++)
+			printf("%u,", entries[i].data[k]);
+		puts("");
+	}
+
+	// Finally, calculate compressed size
+	u32 compsize = 0;
+	for (i = 0; i < len;) {
+		for (k = 0; k < numentries; k++) {
+			if (i + entries[k].data.size() >= len)
+				continue;
+			if (!memcmp(&mem[i], &entries[k].data[0], entries[k].data.size())) {
+				compsize++;
+				i += entries[k].data.size();
+				goto next;
+			}
+		}
+
+		printf("Not found at %u\n", i);
+		abort();
+
+		next:;
+	}
+	if (i != len)
+		abort();
+
 	free(mem);
+	free(memmap);
 	return 0;
 }
